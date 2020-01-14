0ca4a284998deb74a53331b531c9d6ed42d784c8
Izzy <beta5553@gmail.com>
2020-01-14 16:58:12 -0700

    adding botton to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 class Header extends Component {
 
+    switcher = () => {
+        console.log('This was clciked')
+    }
+
     render () {
         return (
             <div>
                 <h1>Header aqui!</h1>
+                <button onClick={this.switcher}>Log something</button>
             </div>
         )
     }
-
 }
 
 export default Header
